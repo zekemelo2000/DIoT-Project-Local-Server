591e8f8ffc0374199d7dfc16c759a527a75c5bda
zekemelo2000 <zekemelo2000@gmail.com>
2026-01-22 21:27:54 -0800

    moved routes to Routes.py

--- app.py
+++ app.py
@@ -6,40 +6,7 @@ import MongoBootStrap
 from quart import Quart
 app = Quart(__name__)
 
-@app.route("/")
-async def hello():
-    return "<h1>Welcome to my Quart Server</h1><p>The server is active!</p>"
 
-# @app.route('/authenticate', methods=['POST'])
-# def index():
-#     if request.method =='POST':
-#         username = request.json.get('API')
-#         password = request.json.get('secret')
-#     else:
-#         return '500'
-@app.route('/ESP32', methods=['GET', 'POST'])
-async def esp32():
-    if request.method == 'POST':
-        print('POST was requested.')
-        data = request.get_json()
-
-        request_number = data.get('requestNumber')
-        print('Request Number: ' + request_number + '\n')
-
-        return '200'
-    else:
-        return '500'
-
-@app.route('/RemotePair', methods=['GET', 'POST'])
-async def remote_pair():
-    if request.method == 'POST':
-        print('POST was requested.')
-        return '200'
-    elif request.method == 'GET':
-        print('GET was requested.')
-        return '200'
-    else:
-        return '500'
 
 async def input_loop():
     ssid_in_use = ""
