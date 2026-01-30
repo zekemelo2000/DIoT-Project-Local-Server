554ccd58f9f3db51eca568d82d02c8f485d7dee5
zekemelo2000 <zekemelo2000@gmail.com>
2026-01-29 19:05:29 -0800

    removed strict validation

--- mongo_bootstrap.py
+++ mongo_bootstrap.py
@@ -4,24 +4,18 @@ api_keys_validation = {
   "$jsonSchema": {
     "bsonType": "object",
     "required": [
-      "Server"
       "API key",
       "Hashed secret"
     ],
     "properties": {
-      "Server": {
-        "bsonType" : "string"
-      },
       "API key": {
         "bsonType": "string"
       },
-      "Secret": {
+      "Hashed secret": {
         "bsonType": "string"
       }
     }
   },
-  "validationLevel": "strict",
-  "validationAction": "error"
 }
 
 dummy_api_keys_validation = {
@@ -35,13 +29,11 @@ dummy_api_keys_validation = {
       "API key": {
         "bsonType": "string"
       },
-      "Secret": {
+      "Hashed secret": {
         "bsonType": "string"
       }
     }
   },
-  "validationLevel": "strict",
-  "validationAction": "error"
 }
 
 wifi_connections_validation = {
@@ -60,8 +52,6 @@ wifi_connections_validation = {
       }
     }
   },
-  "validationLevel": "strict",
-  "validationAction": "error"
 }
 
 api_passport_validation = {
@@ -73,9 +63,6 @@ api_passport_validation = {
       "Secret"
     ],
     "properties": {
-      "Server": {
-          "bsonType" : "string"
-      },
       "API key": {
         "bsonType": "string"
       },
@@ -84,61 +71,59 @@ api_passport_validation = {
       }
     }
   },
-  "validationLevel": "strict",
-  "validationAction": "error"
 }
 
 
-def initialize_database(db):
+async def initialize_database(db):
     try:
-        db.create_collection("api_keys", validator=api_keys_validation )
+        await db.create_collection("api_keys", validator=api_keys_validation )
         print("Collection 'api_keys' created.")
     except CollectionInvalid:
         print("Collection 'api_keys' already exists.")
 
     try:
-        db.create_collection("dummy_api_keys", validator=dummy_api_keys_validation )
+        await db.create_collection("dummy_api_keys", validator=dummy_api_keys_validation )
         print("Collection 'dummy_api_keys' created.")
     except CollectionInvalid:
         print("Collection 'dummy_api_keys' already exists.")
 
     try:
-        db.create_collection("wifi_connections", validator=wifi_connections_validation )
+        await db.create_collection("wifi_connections", validator=wifi_connections_validation )
         print("Collection 'wifi_connections' created.")
     except CollectionInvalid:
         print("Collection 'wifi_connections' already exists.")
 
     try:
-        db.create_collection("api_passport_validation", validator=api_passport_validation )
+        await db.create_collection("api_passport_validation", validator=api_passport_validation )
         print("Collection 'api_passport_validation' created.")
     except CollectionInvalid:
         print("Collection 'api_passport_validation' already exists.")
 
-def drop_database(db):
+async def drop_database(db):
     collection_name = "api_keys"
-    if collection_name in db.list_collection_names():
-        db.drop_collection(collection_name)
+    if collection_name in await db.list_collection_names():
+        await db.drop_collection(collection_name)
         print(f"Collection {collection_name} dropped.")
     else:
         print(f"Collection {collection_name} does not exist.")
 
     collection_name = "dummy_api_keys"
-    if collection_name in db.list_collection_names():
-        db.drop_collection(collection_name)
+    if collection_name in await db.list_collection_names():
+        await db.drop_collection(collection_name)
         print(f"Collection {collection_name} dropped.")
     else:
         print(f"Collection {collection_name} does not exist.")
 
     collection_name = "wifi_connections"
-    if collection_name in db.list_collection_names():
-        db.drop_collection(collection_name)
+    if collection_name in await db.list_collection_names():
+        await db.drop_collection(collection_name)
         print(f"Collection {collection_name} dropped.")
     else:
         print(f"Collection {collection_name} does not exist.")
 
     collection_name = "api_passport_validation"
-    if collection_name in db.list_collection_names():
-        db.drop_collection(collection_name)
+    if collection_name in await db.list_collection_names():
+        await db.drop_collection(collection_name)
         print(f"Collection {collection_name} dropped.")
     else:
         print(f"Collection {collection_name} does not exist.")
