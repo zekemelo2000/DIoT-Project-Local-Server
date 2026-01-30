c76c94fc6969833c2fd7b07800c39701a1a4e288
zekemelo2000 <zekemelo2000@gmail.com>
2026-01-29 19:04:36 -0800

    fixed db passing logic to input_loop

--- app.py
+++ app.py
@@ -19,15 +19,17 @@ async def server():
     mongo_collection = app.mongo_connection.get_collection("api_keys")
     try:
         entry = await mongo_collection.find_one({"API key": "test"})
-        print(f"API key is {entry['API key']}")
-        print(f"Dummy secret is {entry["Hashed secret"]}")
+        if entry is not None:
+            print(f"API key is {entry['API key']}")
+            print(f"Dummy secret is {entry["Hashed secret"]}")
     except OperationFailure:
         print(f"error has occurred: Authentication required")
-    await app.run_task(host="0.0.0.0", port=8080)
+    finally:
+        await app.run_task(host="0.0.0.0", port=8080)
 
 async def main():
     app.mongo_connection.connect()
-    await asyncio.gather(server(), input_loop.input_loop(app.mongo_connection))
+    await asyncio.gather(server(), input_loop.input_loop(app.mongo_connection.db))
     return 0
 
 if __name__ == '__main__':
